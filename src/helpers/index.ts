faa0e4679fb07e56652e1300d18cb10226327acf
Daenhel <jesus2612g@gmail.com>
2024-01-02 11:33:21 -0500

    First Upload

---
+++ src/helpers/index.ts
@@ -0,0 +1,6 @@
+import crypto from 'crypto';
+const SECRET = 'DAENHEL-REST-API';
+export const random = () => crypto.randomBytes(128).toString('base64');
+export const authentication = (salt: string, password: string) => {
+    return crypto.createHmac('sha256', [salt, password].join('/')).update(SECRET).digest(`hex`);
+};
